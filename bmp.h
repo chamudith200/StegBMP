a1120e319c46cb2726bf0052cf49eae3f91be6b3
chamudith200 <chamudiththenura@gmail.com>
2026-05-23 10:47:02 +0530

    bmp.md

--- bmp.h
+++ bmp.h
@@ -7,10 +7,13 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef uint8_t BMP_color[8];
 
 enum {
-    STG_SUCCESS,
-    STG_FAILURE
+    BMP_SUCCESS,
+    BMP_FAILURE
 };
 
 // BMP Structs
@@ -103,8 +106,8 @@ void BMP_InitHeader(BMP *bmp, int width, int height) {
 * - filename: the destination path/filename of the new BMP file.
 *
 * Returns:
-* - STG_SUCCESS (0) on success.
-* - STG_FAILURE (1) on failure 
+* - BMP_SUCCESS (0) on success.
+* - BMP_FAILURE (1) on failure 
 */ 
 int BMP_CreateFile(BMP *bmp, char *fileName) {
     BMP_FileHeader *fileHeader = &bmp->fileHeader;
@@ -112,7 +115,7 @@ int BMP_CreateFile(BMP *bmp, char *fileName) {
     FILE *fp_bmp = fopen(fileName, "wb");
     if (!fp_bmp) {
         printf("Failed to open %s\n", fileName);
-        return STG_FAILURE;
+        return BMP_FAILURE;
     }
     
     // fileHeader
@@ -135,12 +138,12 @@ int BMP_CreateFile(BMP *bmp, char *fileName) {
 
     fclose(fp_bmp);
     if (DBG) printf("Created : %s\n", fileName);
-    return STG_SUCCESS;
+    return BMP_SUCCESS;
 
     FUNC_FAIL:
     printf("Failed to write: %s\n", fileName);
     fclose(fp_bmp);
-    return STG_FAILURE;
+    return BMP_FAILURE;
 
 }
 
@@ -152,8 +155,8 @@ int BMP_CreateFile(BMP *bmp, char *fileName) {
 * - filename: Filename of the bmp file need to read
 *
 * Returns:
-* - STG_SUCCESS (0) on success.
-* - STG_FAILURE (1) on failure.
+* - BMP_SUCCESS (0) on success.
+* - BMP_FAILURE (1) on failure.
 */ 
 int BMP_ReadFile(BMP *bmp, char *fileName) {
     FILE *fp_bmp = fopen(fileName, "rb");
@@ -190,15 +193,84 @@ int BMP_ReadFile(BMP *bmp, char *fileName) {
         printf("Failed to read pixelData: %s\n", fileName);
         goto FUNC_FAIL;
     }
-
+    
     fclose(fp_bmp);
     if (DBG) printf("BMP \"%s\" read successsfuly.\n", fileName);
-    return STG_SUCCESS;
+    return BMP_SUCCESS;
 
     FUNC_FAIL:
     printf("Failed to read: %s\n", fileName);
     fclose(fp_bmp);
-    return STG_FAILURE;
+    return BMP_FAILURE;
+}
+
+
+//
+int BMP_FillSolid(BMP *bmp, uint8_t color[3]) {
+    uint8_t *pixelData = malloc(bmp->pxSize);
+    if (!pixelData) {
+        printf("Failed to aloocate memory: BMP_PxSolid\n");
+        return BMP_FAILURE;
+    }
+
+    int lineSize = 3 * bmp->infoHeader.Width;
+    uint8_t *solidFill = malloc(lineSize);
+    if (!solidFill) {
+        printf("Failed to aloocate memory: BMP_PxSolid\n");
+        return BMP_FAILURE;
+    }
+    for (int x = 0; x < bmp->infoHeader.Width; x++) {
+        int index = x * 3;
+            solidFill[index + 0] = color[0];
+            solidFill[index + 1] = color[1];
+            solidFill[index + 2] = color[2];
+    }
+
+    for (int y = 0; y < bmp->infoHeader.Height; y++) {
+        int index = lineSize * y + bmp->padding;
+        memcpy(&pixelData[index], solidFill, lineSize);
+    }
+
+    bmp->pixelData = pixelData;
+    return BMP_SUCCESS;
+}
+
+// Needs some polishing
+int BMP_FillGradient(BMP *bmp, uint8_t color1[3], uint8_t color2[3]) {
+    uint8_t *pixelData = malloc(bmp->pxSize);
+    if (!pixelData) {
+        printf("Failed to aloocate memory: BMP_PxSolid\n");
+        return BMP_FAILURE;
+    }
+
+    int lineSize = 3 * bmp->infoHeader.Width + bmp->padding;
+    for (int y = 0; y < bmp->infoHeader.Height; y++) {
+        for (int x = 0; x < bmp->infoHeader.Width; x++) {
+            int index = y * lineSize + x * 3;
+            float ratio = (float)x / (bmp->infoHeader.Width - 1);
+
+            pixelData[index + 0] = color2[0] + (int)((color2[0] - color1[0]) * ratio);
+            pixelData[index + 1] = color2[1] + (int)((color2[1] - color1[1]) * ratio);
+            pixelData[index + 2] = color2[2] + (int)((color2[2] - color1[2]) * ratio);
+        }
+    }
+
+    bmp->pixelData = pixelData;
+    return BMP_SUCCESS;
+}
+
+
+// Some abstracted funcs
+int BMP_CreateSolidFill(char *fileName, int height, int width, uint8_t color[3]) {
+    if (height <= 0 || width <= 0) {
+        printf("Height or width can't be <= 0 : %s\n", fileName);
+        return BMP_FAILURE;
+    }
+    BMP bmp;
+    BMP_InitHeader(&bmp, width, height);
+    BMP_FillSolid(&bmp, color);
+    BMP_CreateFile(&bmp, fileName);
+    free(bmp.pixelData);
 }
 
 #endif
